dfc16763fe2bef34d5ab2221590b4938af6b40f6
mbaljeetsingh766 <bsinghs.007@gmail.com>
2019-12-20 10:44:11 +0530

    add blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }, { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
