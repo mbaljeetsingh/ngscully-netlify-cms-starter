d6d413c78e5e190c17a2f1e8d6003900f826dea7
mbaljeetsingh766 <bsinghs.007@gmail.com>
2019-12-20 11:02:09 +0530

    update home and add prettier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }, { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule),
+  },
+  {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from "./home-routing.module";
-import { HomeComponent } from "./home.component";
-import { MaterialModule } from "../shared/material.module";
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, MaterialModule]
+  imports: [CommonModule, HomeRoutingModule, MaterialModule],
 })
 export class HomeModule {}
