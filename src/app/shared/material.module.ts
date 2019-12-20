002fdf91548233ac5f6e5a40a0e5f02b565b075b
mbaljeetsingh766 <bsinghs.007@gmail.com>
2019-12-20 12:13:50 +0530

    update home page

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+  ],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatListModule],
 })
 export class MaterialModule {}
