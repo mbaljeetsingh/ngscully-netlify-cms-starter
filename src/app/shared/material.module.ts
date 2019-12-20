038bdc41b060f9ff448a5c770855dc42cb0da17d
mbaljeetsingh766 <bsinghs.007@gmail.com>
2019-12-20 12:32:18 +0530

    update blog list design

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -4,6 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [],
   imports: [
@@ -12,7 +13,14 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatButtonModule,
     MatListModule,
+    MatCardModule,
   ],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatListModule],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatCardModule,
+  ],
 })
 export class MaterialModule {}
